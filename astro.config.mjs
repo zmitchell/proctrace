6fa60e32c9748fcde049673f30a0db7558460ba4
Zach Mitchell <zmitchell@fastmail.com>
2024-09-06 13:53:09 -0600

    feat: automatically install node dependencies

--- astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-// https://astro.build/config
-export default defineConfig({
-  integrations: [starlight()]
-});
